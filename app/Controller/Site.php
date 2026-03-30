5103acc59f565a3c3ede4320f724494902c97cd1
Александр Харахордин <gr441_hleev@lab-402-7.tomtit-tomsk.ru>
2026-03-30 12:15:46 +0700

    init project

---
+++ app/Controller/Site.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Controller;
+
+use Model\Post;
+use Src\View;
+
+class Site
+{
+    public function index(): void
+    {
+        echo 'working index';
+    }
+
+    public function hello(): void
+    {
+        echo 'working hello';
+    }
+}
