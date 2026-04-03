0e4e5a38e6919dbafaab0ba70ce269354dc32f37
aleksandr22-creat <aleksandrharahordin36@gmail.com>
2026-04-03 12:36:13 +0700

    сделал вход и регистрацию

--- app/Controller/Site.php
+++ app/Controller/Site.php
@@ -3,18 +3,49 @@
 namespace Controller;
 
 use Model\Post;
+use Src\Request;
 use Src\View;
+use Model\User;
+use Src\Auth\Auth;
+
 
 class Site
 {
-    public function index(): string
+    public function index(Request $request): string
     {
-        $posts = Post::all();
+        $posts = Post::where('id', $request->id)->get();
         return (new View())->render('site.post', ['posts' => $posts]);
     }
 
     public function hello(): string
     {
-        return new View('site.hello',['message' => 'working hello']);
+        return new View('site.hello', ['message' => 'working hello']);
+    }
+
+    public function signup(Request $request): string
+    {
+        if ($request->method === 'POST' && User::create($request->all())) {
+            app()->route->redirect('/go');
+        }
+        return new View('site.signup');
+    }
+
+    public function login(Request $request): string
+    {
+        if ($request->method === 'GET') {
+            return new View('site.login');
+        }
+
+        if (Auth::attempt($request->all())) {
+            app()->route->redirect('/hello');
+        }
+
+        return new View('site.login', ['message' => 'Неправильные логин или пароль']);
+    }
+
+    public function logout(): void
+    {
+        Auth::logout();
+        app()->route->redirect('/hello');
     }
 }
---
+++ config/app.php
@@ -0,0 +1,11 @@
+<?php
+return [
+
+    'auth' => \Src\Auth\Auth::class,
+
+    'identity'=>\Model\User::class,
+
+    'routeMiddleware' => [
+        'auth' => \Middlewares\AuthMiddleware::class,
+    ]
+];
